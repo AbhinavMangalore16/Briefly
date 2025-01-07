646e2367560344a04a60114e118b970dc4de4ee9
AbhinavMangalore16 <abhinavm16104@gmail.com>
2025-01-07 21:48:29 +0530

    data ingestion pipeline integration start

---
+++ src/Briefly/config/configuration.py
@@ -0,0 +1,21 @@
+from src.Briefly.utils.common import read_yaml, create_dirs
+from src.Briefly.constants import *
+
+from src.Briefly.entity import DataIngestionConfig
+
+class ConfigManager: 
+    def __init__(self, config_path: Path = CONFIG_PATH, params_path: Path = PARAMS_PATH):
+        self.config = read_yaml(config_path) 
+        self.params = read_yaml(params_path)
+
+        create_dirs([self.config.artifacts_root])
+    def get_dataingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+        create_dirs([config.root_dir,])
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_url=config.source_url,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir
+        )
+        return data_ingestion_config
