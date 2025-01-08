0a365f07ce5c4854fd0f44183c12e546675853ac
AbhinavMangalore16 <abhinavm16104@gmail.com>
2025-01-08 16:21:45 +0530

    prediction pipeline bug fixed

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ from fastapi.responses import Response
 from src.Briefly.pipeline.prediction_pipeline import PredictionPipeline
 
 
-text:str = "The rapid advancements in artificial intelligence (AI) are reshaping industries around the globe. AI-powered solutions are now being utilized in healthcare to improve diagnostics, in education to personalize learning experiences, and in finance to detect fraudulent transactions. Despite its benefits, the adoption of AI is not without challenges. Concerns about data privacy, ethical considerations, and the displacement of jobs are prevalent. Governments and organizations are actively working on policies to address these issues while ensuring that AI continues to drive innovation. As AI technologies evolve, their potential to solve complex global problems continues to grow, making it crucial for stakeholders to balance innovation with responsibility."
+text:str = """The rapid advancements in artificial intelligence (AI) are reshaping industries around the globe. 
+AI-powered solutions are now being utilized in healthcare to improve diagnostics, in education to personalize learning experiences, 
+and in finance to detect fraudulent transactions. Despite its benefits, the adoption of AI is not without challenges. 
+Concerns about data privacy, ethical considerations, and the displacement of jobs are prevalent. 
+Governments and organizations are actively working on policies to address these issues while ensuring that AI continues to drive innovation. 
+As AI technologies evolve, their potential to solve complex global problems continues to grow, making it crucial for stakeholders to balance innovation with responsibility."""
 
 app = FastAPI()
 
@@ -23,7 +28,7 @@ async def training():
         os.system("python main.py")
         return Response("Training model success!")
     except Exception as ee:
-        return Response(f"Error: {e}")
+        return Response(f"Error: {ee}")
     
 @app.post("/predict")
 async def predict(text):
@@ -32,6 +37,7 @@ async def predict(text):
         summary = pred.predict(text)
         return summary
     except Exception as ee:
+        print(f"Error during prediction: {str(ee)}", file=sys.stderr)
         raise ee
 
 
--- src/Briefly/pipeline/prediction_pipeline.py
+++ src/Briefly/pipeline/prediction_pipeline.py
@@ -8,7 +8,7 @@ class PredictionPipeline:
     def predict(self, input_text):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
         gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
-        pipeline_ml = pipeline("Text Summarization by Briefly", model = self.config.model_path, tokenizer=tokenizer)
+        pipeline_ml = pipeline("summarization", model = self.config.model_path, tokenizer=tokenizer)
         
         print("Dialogue:")
         print(input_text)
